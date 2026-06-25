de64ebb5703dd7ae4e96ca2eecafc761da558e5f
Calcados24h <saccalcados24h@gmail.com>
2026-06-25 18:49:30 -0300

    Add API handler for Bling service

---
+++ api/bling.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  const { path } = req.query;
+  const token = req.headers.authorization;
+
+  try {
+    const url = https://www.bling.com.br/Api/v3/${path};
+    const response = await fetch(url, {
+      method: req.method,
+      headers: {
+        'Authorization': token,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined,
+    });
+
+    const data = await response.json();
+    return res.status(response.status).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
