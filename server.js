0072574d6e101383128103b3bf781d49bde11ec2
Calcados24h <saccalcados24h@gmail.com>
2026-07-18 08:15:56 -0300

    Update refresh and access tokens in server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const http = require('http');
 
 const CLIENT_ID = '31dd8ce7bbc6f81357f77bd708d55d066d5a8e9e';
 const CLIENT_SECRET = '7082a944fa4a4e5776e0cee250bc9ae1fdbf229e62d09e0568774278efcb';
-let refreshToken = 'e5469013bd8fec60b5ece2da11dbf646f5fd4994';
-let accessToken = 'ac4a9c9c4280face7d1204e330a8e0bc3bef336c';
+let refreshToken = 'f0dc15429a7bdd9d50241b3a78b66472bcdda48d';
+let accessToken = 'e0d5bfb3649b721f69a03929ca132959c5710834';
 let tokenExpiry = Date.now() + (5 * 60 * 60 * 1000);
 
 function renewToken() {
