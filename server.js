59ba56ff3e1ef841d3e18756308098afd980cec4
Calcados24h <saccalcados24h@gmail.com>
2026-09-02 19:33:50 -0300

    Update REFRESH_TOKEN value in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const CLIENT_ID = '31dd8ce7bbc6f81357f77bd708d55d066d5a8e9e';
 const CLIENT_SECRET = '7082a944fa4a4e5776e0cee250bc9ae1fdbf229e62d09e0568774278efcb';
-var REFRESH_TOKEN = 'dafc255df8cdc5ca14be258484054a5aeaac06a7';
+var REFRESH_TOKEN = 'dc93de001b662dc618b0d69c56e2606bdc7aaca5';
 
 var accessToken = '';
 var tokenExpiry = 0;
